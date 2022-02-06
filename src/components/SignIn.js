9de8e3cd33c0e704a4611da90153cd716c11225d
Amotul-raheem <amotulraheem1499@gmail.com>
2022-02-06 19:50:48 +0100

    styles has issues

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,63 +2,66 @@ import React, {useState} from "react";
 import "./SignIn.css"
 
 function SignIn() {
-    const [values,setValues] = useState({
+    const [values, setValues] = useState({
         email: "",
-        password:""
+        password: ""
     })
-    function handleChange (e) {
+
+    function handleChange(e) {
         const {name, value} = e.target
         setValues({
             ...values,
-            [name]:value
+            [name]: value
         })
     }
 
-    function handleSubmit (e) {
+    function handleSubmit(e) {
         e.preventDefault()
     }
+
     return (
         <div className="container">
             <div className="form-content-left">
                 <h2 className="park-logo">Park</h2>
             </div>
-            <form className= "form">
+            <div className="form">
                 <h2 className="sign-in">Sign In</h2>
-                <span className="form-input-signup">Don't have an account? <a href="#">Sign Up</a> </span>
-                <div className= "form-inputs">
-                  <label
-                      htmlFor="email"
-                      className="form-label-email">
-                      Email
-                  </label>
+                <form>
+                <div className="form-inputs">
+                    <label
+                        htmlFor="email"
+                        className="form-label-email">
+                        Email
+                    </label>
                     <input
-                        type= "email"
+                        type="email"
                         name="email"
                         className="form-input-email"
                         value={values.email}
-                        onChange={handleChange}
-                    />
+                        onChange={handleChange}/>
                 </div>
-                <div className= "form-inputs">
+                <div className="form-inputs">
                     <label
                         htmlFor="password"
                         className="form-label-password">
                         Password
                     </label>
                     <input
-                        type= "password"
+                        type="password"
                         name="password"
                         className="form-input-password"
                         value={values.password}
                         onChange={handleChange}
-                        size= "50"
+                        size="50"
                     />
                 </div>
                 <span className="form-input-forgot"><a>Forgot Password?</a></span>
-                <button className= "form-input-btn" type= "submit" onClick={handleSubmit}>
+                <button className="form-input-btn" type="submit" onClick={handleSubmit}>
                     <p className="btn-sign-in">Sign In</p>
                 </button>
-            </form>
+                </form>
+                <h5>Don't have an account? <a href="#">Sign Up</a> </h5>
+            </div>
         </div>
     )
 }
