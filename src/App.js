b72002f038ccc93a407c3310eb85468a044ffba2
folaraz <folaraz11@gmail.com>
2022-02-07 21:35:16 +0000

    fixed app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, {Component} from 'react';
 import LandingPage from "./components/landingpage/LandingPage";
 import SignUp from "./components/signup/SignUp";
-import{BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-class App extends Component{
-  render(){
-  return (<Router>
-          <div>
-              <Switch>
-                  <Route exact path={"/"}>
-                      <LandingPage/>
-                  </Route>
-                  <Route path={"/sign-up"}>
-                      <SignUp/>
-                  </Route>
-              </Switch>
-
-          </div>
-      </Router>
-  );
-}
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route exact path={"/"} element={<LandingPage/>}/>
+                    <Route path={"/sign-up"} element={<SignUp/>}/>
+                </Routes>
+            </Router>
+        );
+    }
 }
 
 export default App;
