b4ae6d084c79e2cec67fc84b0179546e51a89b68
folaneem <folaneem@gmail.com>
2022-02-07 18:38:53 +0100

    created the landing page and sign up page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React, {Component} from 'react';
+import LandingPage from "./components/landingpage/LandingPage";
+import SignUp from "./components/signup/SignUp";
+import{BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-const helloWorld = () => {
-  return "Hello World"
-}
-function App() {
-  return (
-    <div>
-        {helloWorld()}
-    </div>
+class App extends Component{
+  render(){
+  return (<Router>
+          <div>
+              <Switch>
+                  <Route exact path={"/"}>
+                      <LandingPage/>
+                  </Route>
+                  <Route path={"/sign-up"}>
+                      <SignUp/>
+                  </Route>
+              </Switch>
+
+          </div>
+      </Router>
   );
+}
 }
 
 export default App;
