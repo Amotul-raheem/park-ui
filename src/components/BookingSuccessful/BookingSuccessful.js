b27028fc3e1f46512467db383865040e7ab920e9
Olajide Abdulrazzaq Folarin <folaraz11@gmail.com>
2022-02-19 18:16:22 +0000

    formatted file

--- src/components/BookingSuccessful/BookingSuccessful.js
+++ src/components/BookingSuccessful/BookingSuccessful.js
@@ -3,25 +3,25 @@ import "./BookingSuccessful.css"
 import {useNavigate} from "react-router-dom";
 import FormButton from "../common/FormButton/FormButton";
 import check from "../../images/check.png";
-import HomePageLogo from "../common/HomeLogo/HomePageLogo";
 
-function BookingSuccessful(){
+function BookingSuccessful() {
     const navigate = useNavigate();
-    return(
+    return (
         <div className={"booking-successful"}>
-<div className={"booking-successful-container"}>
-    <div className="booking-successful-text-container">
-    <img src={check} alt="check-image" className="check-image"/>
-    <h2 className="booking-successful-header">Booking Successfully Done!</h2>
-</div>
-    <div className="booking-successful-button-container">
-        <FormButton
-            name={"Go Back To Home"}
-            onClick={() => navigate("/")}
-        />
-    </div>
-</div>
+            <div className={"booking-successful-container"}>
+                <div className="booking-successful-text-container">
+                    <img src={check} alt="check-image" className="check-image"/>
+                    <h2 className="booking-successful-header">Booking Successfully Done!</h2>
+                </div>
+                <div className="booking-successful-button-container">
+                    <FormButton
+                        name={"Go Back To Home"}
+                        onClick={() => navigate("/")}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
+
 export default BookingSuccessful;
