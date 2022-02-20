fcea15fbca57b348a6d2f766a79f943ce1a1bd75
folaneem <folaneem@gmail.com>
2022-02-20 18:37:09 +0100

    adjusted the styling in BookingSuccessful.css

--- src/components/BookingSuccessful/BookingSuccessful.js
+++ src/components/BookingSuccessful/BookingSuccessful.js
@@ -7,18 +7,20 @@ import check from "../../images/check.png";
 function BookingSuccessful() {
     const navigate = useNavigate();
     return (
-        <div className={"booking-successful"}>
-            <div className={"booking-successful-container"}>
-                <div className="booking-successful-text-container">
-                    <img src={check} alt="check-image" className="check-image"/>
-                    <h2 className="booking-successful-header">Booking Successfully Done!</h2>
-                    <FormButton
-                        className={"booking-successful-button-container"}
-                        name={"Go Back To Home"}
-                        onClick={() => navigate("/")}
-                    />
+        <div className="booking-successful">
+            <div className="booking-successful-container">
+                <div className="booking-successful-form">
+                    <div className="booking-successful-text-container">
+                        <img src={check} alt="check-image" className="check-image"/>
+                        <h2 className="booking-successful-header">Booking Successfully Done!</h2>
+                    </div>
+                    <div className="booking-successful-button-container">
+                        <FormButton
+                            name={"Go Back To Home"}
+                            onClick={() => navigate("/")}
+                        />
+                    </div>
                 </div>
-
             </div>
         </div>
     )
