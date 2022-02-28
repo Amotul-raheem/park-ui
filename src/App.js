7e06e8f5da2b791fcb8eb8304d7f32b9fb2e96b8
folaneem <folaneem@gmail.com>
2022-02-28 19:06:00 +0100

    created booking history

--- src/App.js
+++ src/App.js
@@ -10,21 +10,24 @@ import SignUp from "./components/SignUp/SignUp";
 import ResetPasswordSuccessful from "./components/ResetPasswordSuccessful/ResetPasswordSuccessful";
 import ResetPasswordCheckEmail from "./components/ResetPasswordCheckEmail/ResetPasswordCheckEmail"
 import BookingSuccessful from "./components/BookingSuccessful/BookingSuccessful";
+import BookingHistory from "./components/BookingHistory/BookingHistory";
+import ParkingDetail from "./components/common/ParkingDetail/ParkingDetail";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path={"/"} element={<HomePage/>}/>
+                <Route exact path={"/"} element={<ParkingDetail/>}/>
                 <Route path="/sign-in" element={<SignIn/>}/>
                 <Route path="/reset-password" element={<ResetPassword/>}/>
                 <Route path="/reset-password-successful" element={<ResetPasswordSuccessful/>}/>
                 <Route path="/forgot-password" element={<ForgotPassword/>}/>
                 <Route path="/sign-up" element={<SignUp/>}/>
                 <Route path="/user-profile" element={<UserProfile/>}/>
+                <Route path="/booking-history" element={<BookingHistory/>}/>
                 <Route path="/reset-password-check-email" element={<ResetPasswordCheckEmail/>}/>
                 <Route path="/booking-successful" element={<BookingSuccessful/>}/>
-                
+
             </Routes>
         </Router>
     );
---
+++ src/components/BookingHistory/BookingHistory.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./BookingHistory.css"
+import ParkingDetail from "../common/ParkingDetail/ParkingDetail";
+import SideBar from "../common/SideBar/SideBar";
+import profile from "../../images/profile.png";
+
+function BookingHistory() {
+    return (
+        <div className="booking-history">
+            <div>
+                <SideBar/></div>
+            <div className="booking-history-container">
+
+                <h1 className="booking-header">Parking History</h1>
+
+                <ParkingDetail
+                    label="Spot 5"
+                    checkInTime="Fri,11 Feb,12:30pm"
+                    checkOutTime="Sat,12 Feb, 12:30pm"
+                    price="$99.99"/>
+                <ParkingDetail
+                    label="Spot 5"
+                    checkInTime="Fri,11 Feb,12:30pm"
+                    checkOutTime="Sat,12 Feb, 12:30pm"
+                    price="$99.99"/>
+                <ParkingDetail
+                    label="Spot 5"
+                    checkInTime="Fri,11 Feb,12:30pm"
+                    checkOutTime="Sat,12 Feb, 12:30pm"
+                    price="$99.99"/>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default BookingHistory
