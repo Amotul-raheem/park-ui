85d69d8e9fb437eb73b79384ef020b8c6033024e
folaneem <folaneem@gmail.com>
2022-03-06 18:14:56 +0100

    adjusted booking history again

--- src/components/BookingHistory/BookingHistory.js
+++ src/components/BookingHistory/BookingHistory.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./BookingHistory.css"
+import YearPicker from "react-year-picker";
 import ParkingDetail from "../common/ParkingDetail/ParkingDetail";
 import SideBar from "../common/SideBar/SideBar";
 
 function BookingHistory() {
+
     return (
         <div className="booking-history">
             <div>
                 <SideBar/></div>
+
             <div className="booking-history-container">
 
-                <h1 className="booking-header">Parking History</h1>
 
+                <h1 className="booking-header">Parking History</h1>
+                <YearPicker className="year-picker"/>
                 <ParkingDetail
                     label="Spot 5"
                     checkInTime="11 Feb,12:30pm"
