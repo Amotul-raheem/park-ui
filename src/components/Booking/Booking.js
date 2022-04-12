bce6a3a84f6e4bb718726b9f769bc6619a7b0019
Amotul-raheem <amotulraheem1499@gmail.com>
2022-04-12 14:15:28 +0100

    cleaned up the code

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -9,9 +9,6 @@ import ParkDescription from "./ParkDescription/ParkDescription";
 import ProfileNav from "../common/ProfileNav/ProfileNav";
 import BookingModal from "../common/Modal/BookingModal";
 import {BOOKING_PATH} from "../constants/UrlPaths";
-import {getToken} from "../Utils/TokenUtils";
-import axios from "axios";
-import {GET_PARK_SPOTS_ENDPOINT} from "../constants/Endpoints";
 import {DEFAULT_ERROR_MESSAGE} from "../constants/ErrorMessage";
 import {getParkSpots, transformParkSpots} from "../Utils/BookingUtil";
 
@@ -19,7 +16,7 @@ import {getParkSpots, transformParkSpots} from "../Utils/BookingUtil";
 function Booking() {
     let navigate = useNavigate();
     const [showModal, setShowModal] = useState(false)
-    const [bookingSuccessful, isBookingSuccessful] = useState(null)
+    const [bookingSuccessful, setBookingSuccessful] = useState(null)
     const [parkSpots, setParkSpots] = useState([])
     const [checkInTime, setCheckInTime] = useState(new Date());
     const [checkOutTime, setCheckOutTime] = useState(checkInTime);
